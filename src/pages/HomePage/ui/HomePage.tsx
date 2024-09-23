0066cd83cf7e24de8f332b919dee0d2ab7b91e05
Ivsmcrew <Ivsmcrew@gmail.com>
2024-09-23 22:06:02 +0300

    refactor: delete reduntant code

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,22 +1,9 @@
-import cls from "./HomePage.module.scss"
+import style from "./HomePage.module.scss"
 
 const HomePage = () => {
 	return (
-		<div className={cls.homepage}>
-			<p className={cls.font}>
-				Hello from{" "}
-				<a
-					href="https://github.com/yunglocokid"
-					target="_blank"
-					className={cls.link}
-				>
-					yunglocokid
-				</a>
-			</p>
-			<pre className={cls.hint}>
-				You can edit <span className={cls.path}>src/pages/HomePage</span> to
-				start {"<3"}!<br />
-			</pre>
+		<div className={style.homepage}>
+			Hello world
 		</div>
 	)
 }
